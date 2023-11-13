4d97a1a4e3aaf73479580a07c93e06c63caa7177
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-11-13 14:56:40 +0300

    feat: implemented common horoscope

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const horoscopeNavItems = document.querySelectorAll('.horoscope-nav__item');
+
+horoscopeNavItems.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    horoscopeNavItems.forEach((item) => item.classList.remove('active'));
+    event.currentTarget.classList.add('active');
+  });
+});
